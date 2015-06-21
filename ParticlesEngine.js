738b2716f2571a58d6d3bbd46708f4c146781bea
Marian Gibala <marian.gibala@eukleia-training.com>
2015-06-21 16:52:29 +0100

    delete image background

--- ParticlesEngine.js
+++ ParticlesEngine.js
@@ -182,113 +182,66 @@
 
 var background = (function () {
 
-    var background = {};
+  var background = {};
 
-    background.init = function () {
+  background.init = function () {
 
-        // ----------------------------------------------------
-        // Select background type to create //
-        //-----------------------------------------------------
+    // Select background type to create
+    var selectBackgroundType = function (type) {
 
-        var selectBackgroundType = function (type) {
+      if (type === "gradient") {
 
+        createBackgroundGradient();
 
-            if (type === "gradient") {
+      } 
+    };
 
-                createBackgroundGradient();
+    // CSS3 Radial Gradient
+    var createBackgroundGradient = function () {
 
-            }
-            else if (type === "image") {
+      var finalValue = "";
+      var fallbackColor;
 
-                createBackgroundImage();
+      for (var property in options.backgroundColors) {
 
-            }
+        if (typeof fallbackColor === "undefined") fallbackColor = options.backgroundColors[property].color;
 
+        // loop only thorough own properties
+        if (options.backgroundColors.hasOwnProperty(property)) {
+
+          // generate CSS code
+
+          finalValue += "radial-gradient(circle at " +
+
+            options.backgroundColors[property].positionX + "% " +
+            options.backgroundColors[property].positionY + "%, #" +
+            options.backgroundColors[property].color +
+
+            ", transparent 100%),"
         }
+      }
 
-        // ----------------------------------------------------
-        // CSS3 Radial Gradient //
-        //-----------------------------------------------------
+      // remove last comma ","
+      finalValue = finalValue.slice(0, -1)
 
-        var createBackgroundGradient = function () {
+      container.style.background = "#" + fallbackColor;
+      container.style.backgroundImage = finalValue;
 
-            var finalValue = "";
-            var fallbackColor;
+    };
 
-            for (var property in options.backgroundColors) {
+    // Facade function //
 
-                if (typeof fallbackColor === "undefined") fallbackColor = options.backgroundColors[property].color;
+    var createBackground = function () {
 
-                // loop only thorough own properties
-                if (options.backgroundColors.hasOwnProperty(property)) {
+      if (options.background !== null) selectBackgroundType(options.background);
 
-                    // generate CSS code
+    };
 
-                    finalValue += "radial-gradient(circle at " +
+    eventBus.subscribe("init", createBackground)
 
-                        options.backgroundColors[property].positionX + "% " +
-                        options.backgroundColors[property].positionY + "%, #" +
-                        options.backgroundColors[property].color +
+  };
 
-                        ", transparent 100%),"
-
-                }
-
-            }
-
-            // remove last comma ","
-            finalValue = finalValue.slice(0, -1)
-
-
-
-            container.style.background = "#" + fallbackColor;
-            container.style.backgroundImage = finalValue;
-
-
-        }
-
-        // ----------------------------------------------------
-        // Image background //
-        //-----------------------------------------------------
-
-
-        var createBackgroundImage = function () {
-
-            container.style.backgroundImage = "url(img/wallpaper.jpg)";
-            container.style.backgroundPosition = "center center";
-            container.style.backgroundSize = "cover";
-
-        }
-
-
-        // ----------------------------------------------------
-        // Facade function //
-        //-----------------------------------------------------
-
-
-        var createBackground = function () {
-
-
-            if (options.background !== null) selectBackgroundType(options.background);
-
-
-        }
-
-
-        // ----------------------------------------------------
-        // Subscribe init event //
-        //-----------------------------------------------------
-
-
-        eventBus.subscribe("init", createBackground)
-
-
-    }
-
-
-
-    return background
-
+  return background
 
 }());
 
